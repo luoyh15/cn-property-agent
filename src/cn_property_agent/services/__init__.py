d738e070c2eb9c01377b3a406565969263916f95
Luo Yuhang <luoyh22@outlook.com>
2026-09-01 14:38:16 +0800

    Export community resolver service

---
+++ src/cn_property_agent/services/__init__.py
@@ -0,0 +1,17 @@
+from .community_resolver import (
+    CommunityResolution,
+    CommunityResolutionCandidate,
+    CommunityResolutionRequest,
+    CommunityResolver,
+    RepositoryCommunityResolver,
+    ResolutionStatus,
+)
+
+__all__ = [
+    "CommunityResolution",
+    "CommunityResolutionCandidate",
+    "CommunityResolutionRequest",
+    "CommunityResolver",
+    "RepositoryCommunityResolver",
+    "ResolutionStatus",
+]
